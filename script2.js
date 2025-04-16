634841134de9069c15dd57c0283a51d7d63552cd
abhishekk119 <79880483+abhishekk119@users.noreply.github.com>
2025-04-16 14:15:35 +0530

    Delete script2.js

--- script2.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementById('hamburger-icon');
-const navLinks = document.getElementById('links');
-
-toggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
