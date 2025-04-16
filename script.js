205cfdca5b91d44c0a7375fb3dc4d86d53bf61ca
abhishekk119 <79880483+abhishekk119@users.noreply.github.com>
2025-04-16 14:16:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('hamburger-icon').addEventListener('click', () => {
+    document.getElementById('menu-and-button').classList.toggle('active');
+});
+
+document.getElementById('btn').addEventListener('click', () => {
+    window.location.href = 'tel:+919332522166'; // Replace with your number
+});
