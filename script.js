2cc2be868e3daae63934871b8919ada521f0c671
abhishekk119 <79880483+abhishekk119@users.noreply.github.com>
2025-04-17 12:55:01 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('hamburger-icon').addEventListener('click', () => {
-    document.getElementById('hamburger-icon').classList.toggle('active');
-    document.getElementById('menu-and-button').classList.toggle('active');
-});
-
-document.getElementById('btn').addEventListener('click', () => {
-    window.location.href = 'tel:+919332522166'; // Replace with your number
-});
